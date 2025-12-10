32a77200f671de1b1d7ef947529a5ba0deb7bd41
manas24103 <manasgupta24103@gmail.com>
2025-12-10 12:58:28 +0530

    some minor

--- CuraMind/client/src/services/api.js
+++ CuraMind/client/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
